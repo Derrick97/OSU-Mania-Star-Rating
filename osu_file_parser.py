01a2d9d8023c3ebcda8c33e176e7742711cc5604
Wolf Derrick <oscar-rong@hotmail.com>
2019-04-05 23:53:29 +0100

    Gather all note data into 4 arrays: note_start, note_end, not_type, column/

--- osu_file_parser.py
+++ osu_file_parser.py
@@ -2,56 +2,13 @@ import numpy as np;
 
 #...Why does python 3.x change next to __next__......
 
-# Read metadata from .osu file.
-def read_metadata(f, line):
-    if "[Metadata]" in line:
-        while "[Difficulty]" not in line:
-            print(line, end = "")
-            line = f.__next__()
-
-# Read mode: key count.
-def read_column_number(f, line):
-    column_number = -1
-    if "CircleSize:" in line:
-        temp = line.strip()
-        column_number = temp[-1]
-        line = f.__next__()
-    return string_to_int(column_number)
 
 def string_to_int(str):
     return int(float(str))
 
-def read_Timing_Points(f, line):
-    if "[TimingPoints]" in line:
-        line = f.__next__()
-        params = object_line.split(",")
-        offset = string_to_int(params[0])
-        # mpb = 60000 / bpm...
-        mpb = string_to_int(params[1])
-        # meter: number of beats in a measure. aka 节拍.
-        meter = string_to_int(params[2])
-        # Other parameters are not important for measuring difficulty.
 
-
-# Main function for parsing note data.
-# https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osu_(file_format)
-def read_note(f, line, column_number):
-    if "[HitObjects]" in line:
-        line = f.__next__()
-        while line != None:
-            parse_hit_object(f, line, column_number)
-            line = f.__next__()
-
-# Helper function for read_note().
-def parse_hit_object(f, object_line, column_number):
-    params = object_line.split(",")
-    column = string_to_int((params[0]))
-    column_width = int(512/column_number)
-    column = int(column / column_width)
-    starting_point = int(params[2])
-    note_type = int(params[3])
-    last_param_chunk = params[5].split(":")
-    note_end = int(last_param_chunk[0])
+def collect_data(data, new_datum):
+    data = data.append(new_datum)
 
 # Parser Class that can be used on other class.
 class parser():
@@ -59,16 +16,84 @@ class parser():
         # Need to find some way to escape \.
         #self.file_path = file_path.replace("\\", "\\\\")
         self.file_path = file_path
-        self.column_number = -1
+        self.column_count = -1
+        self.columns = []
+        self.note_starts = []
+        self.note_ends = []
+        self.note_types = []
+
     def process(self):
-        with open(self.file_path, "r+") as f:
+        with open(self.file_path, "r+", encoding = 'utf-8') as f:
             try:
                 for line in f:
-                    read_metadata(f, line)
-                    temp = read_column_number(f, line)
-                    if temp != -1:
-                        self.column_number = temp
-                    if self.column_number != -1:
-                        read_note(f, line, self.column_number)
+                   self.read_metadata(f, line)
+                   temp = self.read_column_count(f, line)
+                   if temp != -1:
+                       self.column_count = temp
+                   if self.column_count != -1:
+                       self.read_note(f, line, self.column_count)
+
             except StopIteration:
+                # Test for note data.
+                print(self.columns[:20])
+                print(self.note_starts[:20])
                 pass
+
+    # Read metadata from .osu file.
+    def read_metadata(self, f, line):
+        if "[Metadata]" in line:
+            while "[Difficulty]" not in line:
+                print(line, end = "")
+                line = f.__next__()
+
+    # Read mode: key count.
+    def read_column_count(self, f, line):
+        column_count = -1
+        if "CircleSize:" in line:
+            temp = line.strip()
+            column_count = temp[-1]
+            line = f.__next__()
+        return string_to_int(column_count)
+
+    def read_Timing_Points(self, f, line):
+        if "[TimingPoints]" in line:
+            line = f.__next__()
+            params = object_line.split(",")
+            offset = string_to_int(params[0])
+            # mpb = 60000 / bpm...
+            mpb = string_to_int(params[1])
+            # meter: number of beats in a measure. .
+            meter = string_to_int(params[2])
+            # Other parameters are not important for measuring difficulty.
+
+
+    # Main function for parsing note data.
+    # https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osu_(file_format)
+    def read_note(self, f, line, column_count):
+        if "[HitObjects]" in line:
+            line = f.__next__()
+            while line != None:
+                self.parse_hit_object(f, line, column_count)
+                line = f.__next__()
+
+    # Helper function for read_note().
+    # Store all note information in 4 arrays: column, type, start, end.
+    # If note_end is 0, the note is a single note, otherwise a hold.
+    def parse_hit_object(self, f, object_line, column_count):
+        params = object_line.split(",")
+        column = string_to_int((params[0]))
+        column_width = int(512/column_count)
+        column = int(column / column_width)
+        collect_data(self.columns, column)
+
+        note_start = int(params[2])
+        collect_data(self.note_starts, note_start)
+
+        # 1: single note
+        # 128: Hold(LN)
+        note_type = int(params[3])
+        collect_data(self.note_types, note_type)
+
+        last_param_chunk = params[5].split(":")
+        note_end = int(last_param_chunk[0])
+        collect_data(self.note_ends, note_end)
